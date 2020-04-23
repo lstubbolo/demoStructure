23a4b6f7204d6d6415c0895dab2046a9f48865b3
lstubbolo <lstubbol@udel.edu>
2020-04-23 14:54:03 -0400

    First Commit!

---
+++ PiResponses.py
@@ -0,0 +1,81 @@
+import time
+
+
+#   The puprose of this is to encapsulate the set of functions that
+#   that represent the Pi's ability to respond to external stimuli
+#   that occur during loop execution
+
+#   These responses can originate from Firebse (because the pi is checking)
+#   ***??????? or possibly from somewhere else??? Stop writing and code Louis
+
+
+#   check firebase for recent commands
+def checkFirebase():
+    print("\tin PiResponses.checkFirebase")
+    print("\t\tChecking Firebase for commands")
+
+    #   dummy delay
+    time.sleep(.5)
+
+     
+
+    fbResponse = ("nope", "nope")
+
+    return fbResponse
+
+
+#   this is the function that interacts with the outside functions
+def respInterf(action_arg = "nope", obj_arg = "nope", external_Flag = False):
+    print("\nin PiResponses.interface")
+
+    #   Action is the thing that has to happen, object is the thing it has to happen to
+
+    #   check if this function was called externally
+    #   ->  most calls will be done internally, checking for kill signals from Firebase
+    #   ->  external flag comes from user input on the Pi
+
+    #   if this function was called internally to check for firebase commands
+    if external_Flag == False:
+        (action, obj) = checkFirebase()
+
+    #  Flag should only be true if set by user input
+    else:
+        (action, obj) = (action_arg, obj_arg)
+
+
+    #   list comprehension for handling firebase input
+    actions = {"nope": noResp, "Stop": stopRun, "Start_Run": startRun, "Run_Once": runOnce}
+
+    #   call the action function with the object to perform the action on as the argument; save status
+    status = actions[action](obj)
+
+    print(f"\tResponse was")
+    return status
+
+
+def noResp(obj):
+    print(f"\tNo action needed for '{obj}'")
+
+    return "nope"
+
+
+def stopRun(obj):
+    print(f"\nstopping run for {obj}...")
+    time.sleep(.5)
+    print(f"\tStand-in for handling termination for {obj}")
+    return "Stopped"
+
+
+def startRun(obj):
+    print(f"\nstarting run for {obj}")
+    time.sleep(.5)
+    print(f"\tStand-in for handling Starting for {obj}")
+    return "Stopped"
+
+
+def runOnce(obj):
+    print(f"\nrunning once for {obj}")
+    time.sleep(.5)
+    print(f"\tStand-in for handling Starting for {obj}")
+    return "Ran_Once"
+
---
+++ controller.py
@@ -0,0 +1,42 @@
+from OCR_Controller import menu_OCR
+
+
+
+def Settings():
+    print(f"\nSETTINGS ->\tThis function has access to settings json files")
+
+    print("In Controller from Settings, going back to Main Menu")
+
+def OCR():
+    print("You entered OCR!")
+    menu_OCR()
+    print("In Controller from OCR, going back to Main Menu")
+
+def Audio():
+    print("You entered Audio!")
+    print("In Controller from Audio, going back to Main Menu")
+
+
+def EXIT():
+    print("Have a nice day!")
+
+    exit(0)
+
+
+# main
+if __name__ == "__main__":
+    menuString = "\nMAIN MENU\n1)\tSettings\n2)\tOCR\n3)\tAudio\n4)\tEXIT\n"
+
+    options = {1: Settings, 2: OCR, 3: Audio, 4: EXIT}
+    print("In the master File")
+
+    userInput = -1
+
+
+    while userInput != 4:
+        print(menuString)
+        userInput = int(input("Enter 1-4: "))
+        #userInput = 0
+        print(f"You Entered: {userInput}\n")
+        options[userInput]()
+
