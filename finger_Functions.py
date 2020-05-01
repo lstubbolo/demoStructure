902d0c32e4464c2523bfbe16756cb87e0c54966a
lstubbolo <lstubbol@udel.edu>
2020-05-01 13:08:37 -0400

    made comments line up

--- finger_Functions.py
+++ finger_Functions.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 from time import sleep
-GPIO.setmode(GPIO.BOARD) # Set GPIO usage
-GPIO.setup(32, GPIO.OUT)  # Set up pin 32 for GPIO usage
-pwm=GPIO.PWM(32, 50)      # Set up pwm on pin 32
-pwm.start(0)             # Set initial pwm of 0
-GPIO.setwarnings(False)  # Disable GPIO warnings
+GPIO.setmode(GPIO.BOARD)    # Set GPIO usage
+GPIO.setup(32, GPIO.OUT)    # Set up pin 32 for GPIO usage
+pwm = GPIO.PWM(32, 50)      # Set up pwm on pin 32
+pwm.start(0)                # Set initial pwm of 0
+GPIO.setwarnings(False)     # Disable GPIO warnings
 
 #####    Moves finger to starting, retracted position with value of 1
 def startPos():
