a0080ee66b4602fad9af163b5b47f1f76990e3d8
nodeSmart <udelee@udel.edu>
2020-05-01 15:38:05 -0400

    update

--- finger_Functions.py
+++ finger_Functions.py
@@ -1,44 +1,42 @@
+import RPi.GPIO as GPIO
 from time import sleep
-
-try:
-    import RPi.GPIO as GPIO
-except ModuleNotFoundError:
-    print('GPIO Library Cannot Be Used On This System')
-
-try:
-    print("the code below me is attempting to execute")
-    GPIO.setmode(GPIO.BOARD)    # Set GPIO usage
-    GPIO.setup(32, GPIO.OUT)    # Set up pin 32 for GPIO usage
-    pwm = GPIO.PWM(32, 50)      # Set up pwm on pin 32
-    pwm.start(0)                # Set initial pwm of 0
-    GPIO.setwarnings(False)     # Disable GPIO warnings
-    print("the code above me just executed")
-except NameError:
-    print('GPIO Library Cannot Be Used On This System')
 
 #####    Moves finger to starting, retracted position with value of 1
 def startPos():
-    try:
-        dutyStart = 1 / 18 + 2
-        GPIO.output(32, True)
-        pwm.ChangeDutyCycle(dutyStart)
-        sleep(0.3)
-        GPIO.output(32, False)
-        pwm.ChangeDutyCycle(0)
-    except NameError:
-        print("*Finger-Move-Start*")
+    print("print statement")
+    GPIO.setmode(GPIO.BOARD) # Set GPIO usage
+    GPIO.setup(32, GPIO.OUT)  # Set up pin 32 for GPIO usage
+    pwm=GPIO.PWM(32, 50)      # Set up pwm on pin 32
+    pwm.start(0)             # Set initial pwm of 0
+    GPIO.setwarnings(False)  # Disable GPIO warnings
+    GPIO.output(32, True)
+    pwm.ChangeDutyCycle(2.055)
+    sleep(0.5)
+    GPIO.output(32, False)
+    pwm.ChangeDutyCycle(0)
+    GPIO.cleanup()
     
 #####    Moves finger to the press position with value of 50
 def pressPos():
-    try:
-        dutyPress = 50 / 18 + 2
-        GPIO.output(32, True)
-        pwm.ChangeDutyCycle(dutyPress)
-        sleep(0.3)
-        GPIO.output(32, False)
-        pwm.ChangeDutyCycle(0)
-    except NameError:
-        print("*Finger-Move-End*")
+    print("print statement")
+    GPIO.setmode(GPIO.BOARD) # Set GPIO usage
+    GPIO.setup(32, GPIO.OUT)  # Set up pin 32 for GPIO usage
+    pwm=GPIO.PWM(32, 50)      # Set up pwm on pin 32
+    pwm.start(0)             # Set initial pwm of 0
+    GPIO.setwarnings(False)  # Disable GPIO warnings
+    GPIO.output(32, True)
+    pwm.ChangeDutyCycle(4.777)
+    sleep(0.5)
+    GPIO.output(32, False)
+    pwm.ChangeDutyCycle(0)
+    GPIO.cleanup()
 
-
-#GPIO.cleanup()     ##  May or may not need this line after sequence of execution(s) depending on other GPIO setup
+startPos()
+pressPos()
+startPos()
+pressPos()
+startPos()
+pressPos()
+startPos()
+    
+  ##  May or may not need this line after sequence of execution(s) depending on other GPIO setup
