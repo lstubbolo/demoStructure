6dfc1c2e32de33ff73c4492d0daa48f5a681c78a
lstubbolo <lstubbol@udel.edu>
2020-05-01 18:18:13 -0400

    change font

--- guiTest.py
+++ guiTest.py
@@ -111,10 +111,10 @@ class MainMenu(tk.Frame):
         quit_btn_func = lambda: (test.pre_quit(),
                                  controller.destroy())
 
-        start_stop_ocr_btn          = tk.Button(self, height=90, width=200,  text="OCR", command=ocr_btn_func)
-        start_stop_audio_btn        = tk.Button(self, height=90, width=200,  text="Audio", command=audio_btn_func)
-        settings_btn                = tk.Button(self, height=90, width=200,  text="Settings", command=settings_btn_func)
-        quit_btn                    = tk.Button(self, height=90, width=200,  text="Quit", command=quit_btn_func)
+        start_stop_ocr_btn          = tk.Button(self, height=50, width=100,  text="OCR", command=ocr_btn_func)
+        start_stop_audio_btn        = tk.Button(self, height=50, width=100,  text="Audio", command=audio_btn_func)
+        settings_btn                = tk.Button(self, height=50, width=100,  text="Settings", command=settings_btn_func)
+        quit_btn                    = tk.Button(self, height=50, width=100,  text="Quit", command=quit_btn_func)
 
         start_stop_ocr_btn.pack()
         start_stop_audio_btn.pack()
